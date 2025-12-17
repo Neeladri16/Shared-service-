1216d8406659898bec6e4c24bb6bcd5d7855b14f
Sakshi Singh <ssingh@invessio.com>
2025-12-17 13:39:09 +0530

    remove duplicate file

--- ChatApplication/Backend/ChatBot/obj/Debug/net8.0/ChatBot.AssemblyInfo.cs
+++ ChatApplication/Backend/ChatBot/obj/Debug/net8.0/ChatBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c02f17425151648f4af4054ffe00653a72ecdb07")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
